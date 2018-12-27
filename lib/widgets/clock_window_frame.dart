781ea87db2f63232f190e4ebe50adfd4f5adaa81
Martin Feckie <mfeckie@gmail.com>
2018-12-27 14:12:02 +0800

    Add widow frame widget

---
+++ lib/widgets/clock_window_frame.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class ClockWindowFrame extends CustomPainter {
+  final Paint framePainter;
+  ClockWindowFrame() : this.framePainter = Paint();
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = min(size.width / 2, size.height / 2);
+    final rect = Rect.fromCircle(center: center, radius: radius - 2);
+    final sweep = ( 2 * pi ) * 0.75;
+
+    framePainter.color = Colors.black;
+    framePainter.style = PaintingStyle.stroke;
+    framePainter.strokeWidth = 2.0;
+
+    canvas.drawArc(rect, 0, sweep, true, framePainter);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return this != oldDelegate;
+  }
+
+}
