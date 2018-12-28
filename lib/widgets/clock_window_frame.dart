04e1cf907ecb9de44a6bb819899ece46f6f4a259
Martin Feckie <mfeckie@gmail.com>
2018-12-28 14:07:12 +0800

    Make frame outline more subtle

--- lib/widgets/clock_window_frame.dart
+++ lib/widgets/clock_window_frame.dart
@@ -11,12 +11,12 @@ class ClockWindowFrame extends CustomPainter {
 
     final center = Offset(size.width / 2, size.height / 2);
     final radius = min(size.width / 2, size.height / 2);
-    final rect = Rect.fromCircle(center: center, radius: radius - 2);
+    final rect = Rect.fromCircle(center: center, radius: radius);
     final sweep = ( 2 * pi ) * 0.75;
 
     framePainter.color = Colors.black;
     framePainter.style = PaintingStyle.stroke;
-    framePainter.strokeWidth = 2.0;
+    framePainter.strokeWidth = 1.0;
 
     canvas.drawArc(rect, 0, sweep, true, framePainter);
   }
